960e41891b0223bfad4a9e5fffdef66efa5e419a
Ishchenko-Vladislav <patriq.968@gmail.com>
2024-03-06 15:21:30 +0200

    update

--- src/app/api/post/[postId]/comment/[commentId]/route.ts
+++ src/app/api/post/[postId]/comment/[commentId]/route.ts
@@ -1,19 +1,20 @@
-// import { auth } from "@/lib/auth";
-// import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function POST(req: NextRequest, { params }: { params: { postId: string, commentId: string } }) {
-//     try {
-//         const postId = parseInt(params.postId);
-//         const commentId = parseInt(params.postId);
-//         const session = await auth();
-//         if (!session?.user.id) {
-//           return NextResponse.json({
-//             success: false,
-//             data: null,
-//             message: "You should be logged in.",
-//           });
-//         }
-//     } catch (error) {
-
-//     }
-// }
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { postId: string; commentId: string } }
+) {
+  try {
+    const postId = parseInt(params.postId);
+    const commentId = parseInt(params.postId);
+    const session = await auth();
+    if (!session?.user.id) {
+      return NextResponse.json({
+        success: false,
+        data: null,
+        message: "You should be logged in.",
+      });
+    }
+  } catch (error) {}
+}
